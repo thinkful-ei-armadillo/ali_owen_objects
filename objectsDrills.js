bbf83265d0b4517c10cfbf16fdf0c20ab4e5e11d
Ali Lahrime <alahrime@email.wm.edu>
2019-01-16 11:17:04 -0500

    object creator drill complete

--- objectsDrills.js
+++ objectsDrills.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+function createMyObject() {
+
+return { 
+	foo: 'bar',
+	answerToUniverse: 42,
+	'olly olly': 'oxen free',
+	sayHello: function() {
+		  return 'hello'
+	}
+  }
+}
+
+
