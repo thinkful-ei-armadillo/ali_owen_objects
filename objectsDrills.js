64653fa63701d15978cff0883dddac0ec0eb68a1
owencyr <cyroc@dukes.jmu.edu>
2019-01-16 12:00:33 -0500

    key deleter

--- objectsDrills.js
+++ objectsDrills.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 
@@ -29,9 +30,9 @@ function personMaker() {
     // replace `null` with a function that uses self reference to return
     // full name
     fullName: function () { 
-      return `${this.firstName} ${this.lastName}`
+      return `${this.firstName} ${this.lastName}`;
     }
-  }
+  };
   return person;
 }
 
@@ -72,3 +73,53 @@ function personMaker() {
   }
   console.log('SUCCESS: `updateObject` works correctly!');
 })();
+
+const sampleObj = {
+  foo: 'foo',
+  bar: 'bar',
+  bizz: 'bizz',
+  bang: 'bang',
+};
+
+function keyDeleter(obj) {
+  // your code here
+  delete obj.foo;
+  delete obj.bar;
+  return obj;
+}
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+(function testKeyDeleter() {
+  var obj = keyDeleter({
+    foo: 'foo',
+    bar: 'bar',
+    bizz: 'bizz',
+    bang: 'bang',
+  });
+
+  if (typeof obj !== 'object') {
+    console.error('ERROR: `keyDeleter` must be return an object');
+    return false;
+  }
+  ['foo', 'bar'].forEach(function(key) {
+    if (key in obj) {
+      console.error('`keyDeleter` did not delete the key for ' + key);
+      return false;
+    }
+  });
+  ['bizz', 'bang'].forEach(function(key) {
+    if (!(key in obj && obj[key] === key)) {
+      console.error('`keyDeleter` is deleting keys other than `foo` and `bar`');
+      return false;
+    }
+  });
+  console.log('SUCCESS: `keyDeleter` works correctly!');
+})();
+
