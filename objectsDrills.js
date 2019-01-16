d35149698c92381a32ae43855fb7c3a28a118580
owencyr <cyroc@dukes.jmu.edu>
2019-01-16 11:35:08 -0500

    updateObject function

--- objectsDrills.js
+++ objectsDrills.js
@@ -3,14 +3,23 @@
 
 function createMyObject() {
 
-return { 
-	foo: 'bar',
-	answerToUniverse: 42,
-	'olly olly': 'oxen free',
-	sayHello: function() {
-		  return 'hello'
-	}
-  }
+  return { 
+    foo: 'bar',
+    answerToUniverse: 42,
+    'olly olly': 'oxen free',
+    sayHello: function() {
+      return 'hello';
+    }
+  };
 }
 
+function updateObject(obj) {
+  // if (typeOf obj === 'object') {
+  obj.foo = 'foo';
+  obj.bar = 'bar';
+  obj.bizz = 'bizz';
+  obj.bang = 'bang';
+  return obj;
+}
 
+updateObject();
