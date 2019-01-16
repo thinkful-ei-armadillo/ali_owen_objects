14a13bfd90e40451941277675f38cfa4739e1437
Ali Lahrime <alahrime@email.wm.edu>
2019-01-16 15:42:54 -0500

    We made LOTR character objects

--- objectsDrills.js
+++ objectsDrills.js
@@ -113,4 +113,39 @@ return newMessage;
 console.log(decodeWords('craft block argon meter bells brown croon droop'));
 
 
+function createCharacter(name, nickname, race, origin, attack, defense) {
+
+return {
+  name,
+  nickname,
+  race,
+  origin,
+  attack,
+  defense,
+  describe: function() { return `${name} is a ${race} from ${origin}.` },
+  evaluateFight: function(character) { 
+  let x = character.defense > this.attack ? 0 : this.attack - character.defense;
+  let y = this.defense > character.attack ? 0 : character.attack - this.defense;
+  return `Your opponent takes ${x} damage and you receive ${y} damage`;},
+  }
+}
+
+const LOTR = [];
+LOTR.push(createCharacter('Gandalf the White', 'gandalf', 'Wizard', 'Middle Earth', 10, 6)); 
+LOTR.push(createCharacter('Bilbo Baggins', 'bilbo', 'Hobbit', 'The Shire', 2, 1));
+LOTR.push(createCharacter('Frodo Baggins', 'frodo', 'Hobbit', 'The Shire', 3, 2));
+LOTR.push(createCharacter('Aragorn son of Arathorn', 'aragorn', 'Man', 'Dunnedain', 6, 8));
+LOTR.push(createCharacter('Legolas', 'legolas', 'Elf', 'Woodland Realm', 8, 5));
+
+console.log(LOTR);
+  
+
+
+
+
+
+
+
+
+
 
